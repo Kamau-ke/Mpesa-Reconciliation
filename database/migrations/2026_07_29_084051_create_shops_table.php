63d93a67a0faeb8c9ae42c21b7a5e7098acfff47
Kamau-Ke <kamauwamkuu@gmail.com>
2026-07-29 13:20:11 +0300

    Design DB schemas

---
+++ database/migrations/2026_07_29_084051_create_shops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id');
+            $table->string('till_number');
+            $table->string('passkey');
+            $table->string('location')->nullable();
+            $table->string('currency')->default('KES');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
---
+++ database/migrations/2026_07_29_100440_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('shop_id')->constrained('shops');
+            $table->string('merchant_request_id');
+            $table->string('checkout_request_id');
+            $table->string('mpesa_receipt_number');
+            $table->decimal('amount', 10,2);
+            $table->string('phone_number');
+            $table->timestamp('transaction_date');
+            $table->enum('status', ['pending', 'success', 'failed', 'cancelled']);
+            $table->integer('result_code');
+            $table->string('result_desc');
+            $table->json('raw_payload')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
